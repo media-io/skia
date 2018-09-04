0525578b617ce1c52a2a93f2654fd90bca3d22fc
Marc-Antoine Arnaud <arnaud.marcantoine@gmail.com>
2018-09-04 17:44:44 +0200

    fix ip resolution

--- src/main.rs
+++ src/main.rs
@@ -130,8 +130,8 @@ fn main() {
       };
     upload_ws += &hostname;
     upload_ws += ":";
-    if &hostname != "127.0.0.1" ||
-      &hostname != "localhost" ||
+    if &hostname != "127.0.0.1" &&
+      &hostname != "localhost" &&
       &hostname != "0.0.0.0"  {
       upload_ws += &port;
       upload_ws += "/upload";
